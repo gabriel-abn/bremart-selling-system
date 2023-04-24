882600dfcb15379dad0825aff5465163eec2d9e1
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-04-23 22:26:23 -0300

    feat(Tests): :clown_face: created purchase entity mocks

---
+++ tests/domain/mocks/mock-purchase.ts
@@ -0,0 +1,19 @@
+import { Purchase, PurchaseItem } from "@domain/index";
+
+export const mockPurchaseItem = (): PurchaseItem => {
+  return PurchaseItem.create({
+    id: "ID" + Math.floor(Math.random() * 100).toString(),
+    productId: "PRODUCT" + Math.floor(Math.random() * 100).toString(),
+    quantity: 1,
+    price: 10,
+    uniqueDiscount: 0,
+  });
+};
+
+export const mockPurchase = (): Purchase => {
+  return Purchase.create({
+    id: "ID" + Math.floor(Math.random() * 100).toString(),
+    items: [mockPurchaseItem(), mockPurchaseItem(), mockPurchaseItem()],
+    total: 0,
+  });
+};
