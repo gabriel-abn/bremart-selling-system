74c2c5fa9274467197ea05c0a8d8a9cc14d12312
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-05-17 18:15:43 -0300

    test(Application): :white_check_mark: testing total price discount
    If price greater than 500.00 and payment type in cash, total price should calculate 5% discount.

--- tests/application/purchase/create-purchase-use-case.spec.ts
+++ tests/application/purchase/create-purchase-use-case.spec.ts
@@ -1,37 +1,66 @@
 import { ApplicationError } from "@application/common";
 import { CreatePurchaseUseCase } from "@application/use-cases/purchase";
-import { MockPurchaseRepository } from "@test-application/mocks/repositories";
-import { UUIDGeneratorMock, mockInitialPurchase } from "@test-domain/mocks";
+import { PaymentType } from "@domain/payment-type";
+import {
+  MockPurchaseRepository,
+  MockUserRepository,
+} from "@test-application/mocks/repositories";
+import {
+  UUIDGeneratorMock,
+  mockCompletePurchase,
+  mockPurchaseItem,
+} from "@test-domain/mocks";
 
 const sut = new CreatePurchaseUseCase(
   new UUIDGeneratorMock(),
-  new MockPurchaseRepository()
+  new MockPurchaseRepository(),
+  new MockUserRepository()
 );
+
+const mockProps = mockCompletePurchase({}).props;
 
 describe("Create purchase use case", () => {
   it("should return a purchase", async () => {
     const purchase = await sut.execute({
-      ...mockInitialPurchase(),
+      userId: mockProps.userId,
+      items: mockProps.items,
+      paymentType: mockProps.paymentType,
     });
 
     expect(purchase).toBeTruthy();
   });
 
-  it("should return a purchase with a valid id and total price", async () => {
+  it("should return a purchase with a valid result", async () => {
     const purchase = await sut.execute({
-      ...mockInitialPurchase(),
+      userId: mockProps.userId,
+      items: mockProps.items,
+      paymentType: mockProps.paymentType,
     });
 
     expect(purchase.id).toBeTruthy();
     expect(purchase.total).toBeGreaterThan(0);
+    expect(purchase.userId).toBeTruthy();
   });
 
   it("should throw if purchase has no items", async () => {
     expect(async () => {
       await sut.execute({
-        ...mockInitialPurchase(),
+        userId: mockProps.userId,
+        paymentType: mockProps.paymentType,
         items: [],
       });
     }).rejects.toThrowError(ApplicationError);
+  });
+
+  it("should give 5% discount if payment type is in cash and total price greater than 500", async () => {
+    const purchase = await sut.execute({
+      userId: mockProps.userId,
+      items: [mockPurchaseItem({ price: 600 })],
+      paymentType: PaymentType.CASH,
+    });
+
+    console.log(mockPurchaseItem({ price: 600 }).props);
+
+    expect(purchase.total).toBe(570);
   });
 });
