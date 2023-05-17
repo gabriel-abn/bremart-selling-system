17e33457c5caac70eb07609b0beef6ad751b5fd9
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-05-17 18:13:01 -0300

    test(Domain): :white_check_mark: updated mocks params

--- tests/domain/common/create-entity.spec.ts
+++ tests/domain/common/create-entity.spec.ts
@@ -3,22 +3,22 @@ import { mockCompletePurchase } from "@test-domain/mocks";
 
 describe("Create Entity", () => {
   it("should be able to create an entity", () => {
-    expect(mockCompletePurchase()).toBeInstanceOf(Entity);
+    expect(mockCompletePurchase({})).toBeInstanceOf(Entity);
   });
   it("2 entities should have different IDs", () => {
-    const product1 = mockCompletePurchase();
-    const product2 = mockCompletePurchase();
+    const product1 = mockCompletePurchase({});
+    const product2 = mockCompletePurchase({});
 
     expect(product1.id).not.toEqual(product2.id);
   });
   it("should have a creation date", () => {
-    expect(mockCompletePurchase().getUpdateDates().createdAt).not.toBeNull();
+    expect(mockCompletePurchase({}).getUpdateDates().createdAt).not.toBeNull();
   });
   it("should have a last update date", () => {
-    expect(mockCompletePurchase().getUpdateDates().updatedAt).not.toBeNull();
+    expect(mockCompletePurchase({}).getUpdateDates().updatedAt).not.toBeNull();
   });
   it("should have a last update date equal or greater that creation date", () => {
-    const purchase = mockCompletePurchase();
+    const purchase = mockCompletePurchase({});
 
     expect(
       purchase.getUpdateDates().updatedAt.getTime()
