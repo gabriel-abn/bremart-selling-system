3ec380b978c5b3da56c32d76b5741817fcfe2769
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-05-17 19:35:43 -0300

    test(Application): :white_check_mark: testing exception throw if purchase does not exists

--- tests/application/purchase/get-purchase-use-case.spec.ts
+++ tests/application/purchase/get-purchase-use-case.spec.ts
@@ -1,3 +1,7 @@
+import {
+  GetPurchase,
+  GetPurchaseUseCase,
+} from "@application/use-cases/purchase";
 import { MockPurchaseRepository } from "@test-application/mocks/repositories";
 
 const sut = new GetPurchaseUseCase(new MockPurchaseRepository());
@@ -6,12 +10,18 @@ describe("Get Purchase Use Case", () => {
   it("should return purchase by id", async () => {
     const purchase = await sut.execute({ id: "any_id" });
 
-    expect(purchase).toEqual({ id: "any_id" });
+    expect(purchase).toBeTruthy();
   });
 
   it("should throw if purchase does not exists", async () => {
-    expect(async () => {
-      await sut.execute({ id: "invalid_id" });
-    }).rejects.toThrow("GetPurchaseUseCase: Purchase not found");
+    const purchase = async (): Promise<GetPurchase.Result> => {
+      const response = await sut.execute({ id: "invalid_id" });
+
+      console.log(response);
+
+      return response;
+    };
+
+    expect(purchase).rejects.toThrow("GetPurchaseUseCase: Purchase not found");
   });
 });
