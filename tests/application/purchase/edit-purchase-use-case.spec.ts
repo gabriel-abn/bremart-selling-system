a9d15fcb8b9c6f65a6fb6f37c813896049d3ec14
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-05-17 20:55:12 -0300

    test(Application): :test_tube: created edit purchase use case tests

---
+++ tests/application/purchase/edit-purchase-use-case.spec.ts
@@ -0,0 +1,14 @@
+// make a list of test cases for the edit purchase use case
+// 1. Should call the edit purchase repository with correct values
+// 2. Should throw if invalid purchase id is provided
+// 3. Should throw if invalid purchase data is provided
+// 4. Should throw if any dependency throws
+// 5. Should return the edited purchase on success
+
+describe("Edit Purchase Use Case", () => {
+  it("should throw if invalid id is provided", async () => {});
+  it("should throw if no purchase items provided", async () => {});
+  it("should return the edited purchase on success", async () => {});
+  it("should return the edited purchase if only one field provided", async () => {});
+  it("should remove discount if total price gets below 500.00", async () => {});
+});
