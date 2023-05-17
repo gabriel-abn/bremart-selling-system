b92d06246ce7272ec43bf10586e5f1b249c9cfc8
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-05-17 14:17:17 -0300

    chore(Jest): :wrench: removing deprecated configs
    Removed Jest Globals ts-jest configuration which is deprecated

--- jest.config.ts
+++ jest.config.ts
@@ -65,11 +65,11 @@ export default {
   // globalTeardown: undefined,
 
   // A set of global variables that need to be available in all test environments
-  globals: {
-    "ts-jest": {
-      isolatedModules: true,
-    },
-  },
+  // globals: {
+  //   "ts-jest": {
+  //     isolatedModules: true,
+  //   },
+  // },
 
   // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
   // maxWorkers: "50%",
@@ -166,10 +166,7 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  // testMatch: [
-  //   "**/__tests__/**/*.[jt]s?(x)",
-  //   "**/?(*.)+(spec|test).[tj]s?(x)"
-  // ],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
   // testPathIgnorePatterns: [
@@ -186,7 +183,9 @@ export default {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    "^.+\\.m?[tj]s?$": ["ts-jest", { useESM: true }],
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
