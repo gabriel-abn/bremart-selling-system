43113fd9aaa649a0340e3be8b1fede9028213f3c
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-05-17 18:12:30 -0300

    fix(Tests): :art: using partial type to modify mocks

--- tests/domain/mocks/mock-purchase.ts
+++ tests/domain/mocks/mock-purchase.ts
@@ -1,29 +1,33 @@
-import { Purchase, PurchaseItem } from "@domain/index";
+import {
+  PaymentType,
+  Purchase,
+  PurchaseItem,
+  PurchaseItemProps,
+  PurchaseProps,
+} from "@domain/index";
 
-export const mockPurchaseItem = (): PurchaseItem => {
+export const mockPurchaseItem = (
+  mock: Partial<PurchaseItemProps>
+): PurchaseItem => {
   return PurchaseItem.create({
     id: "ID" + Math.floor(Math.random() * 100).toString(),
     productId: "PRODUCT" + Math.floor(Math.random() * 100).toString(),
     quantity: 1,
-    price: 10,
+    price: mock.price ? mock.price : 50,
     uniqueDiscount: 0,
   });
 };
 
-export const mockInitialPurchase = (): {
-  items: PurchaseItem[];
-  userId?: string;
-} => {
-  return {
-    items: [mockPurchaseItem(), mockPurchaseItem(), mockPurchaseItem()],
-    userId: "1",
-  };
-};
-
-export const mockCompletePurchase = (): Purchase => {
+export const mockCompletePurchase = (
+  mock?: Partial<PurchaseProps>
+): Purchase => {
   return Purchase.create({
-    id: "ID" + Math.floor(Math.random() * 100).toString(),
-    items: [mockPurchaseItem(), mockPurchaseItem(), mockPurchaseItem()],
+    id: mock.id ? mock.id : "ID" + Math.floor(Math.random() * 100).toString(),
+    userId: mock.userId ? mock.userId : "1",
+    items: mock.items
+      ? mock.items
+      : [mockPurchaseItem({}), mockPurchaseItem({}), mockPurchaseItem({})],
+    paymentType: PaymentType.CASH,
     total: 0,
   });
 };
