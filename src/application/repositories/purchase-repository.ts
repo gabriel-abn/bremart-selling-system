1f6eec9319cf0f1c3e3c38732678a2abce3b2ef0
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-05-17 19:36:55 -0300

    feat(Application): :sparkles: added get by id repository method

--- src/application/repositories/purchase-repository.ts
+++ src/application/repositories/purchase-repository.ts
@@ -2,4 +2,5 @@ import { PurchaseProps } from "@domain/purchase";
 
 export interface IPurchaseRepository {
   create(purchase: PurchaseProps): Promise<{ id: string }>;
+  findById(id: string): Promise<PurchaseProps>;
 }
--- tests/application/mocks/repositories/mock-purchase-repository.ts
+++ tests/application/mocks/repositories/mock-purchase-repository.ts
@@ -1,8 +1,22 @@
 import { IPurchaseRepository } from "@application/repositories";
+import { PaymentType } from "@domain/payment-type";
 import { PurchaseProps } from "@domain/purchase";
 
 export class MockPurchaseRepository implements IPurchaseRepository {
   async create(purchase: PurchaseProps): Promise<{ id: string }> {
     return purchase.id ? { id: purchase.id } : { id: "1" };
+  }
+
+  async findById(id: string): Promise<PurchaseProps> {
+    if (id != "any_id") {
+      return null;
+    }
+
+    return {
+      id: "any_id",
+      items: [],
+      total: 0,
+      paymentType: PaymentType.CASH,
+    };
   }
 }
