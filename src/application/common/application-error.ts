c40158038a55c389e6c2dc74ecbefc588efce42e
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-05-17 14:42:57 -0300

    fix(Application): :art: adding class name to error message

--- src/application/common/application-error.ts
+++ src/application/common/application-error.ts
@@ -1,12 +1,5 @@
 export class ApplicationError extends Error {
   constructor(message: string, private className: string) {
-    super(message);
-  }
-
-  toString() {
-    return {
-      className: this.className,
-      message: this.message,
-    };
+    super(className + ": " + message);
   }
 }
