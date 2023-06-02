dba2c9eeb4b93eaaee13250cb94e82452c89f925
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-06-02 16:22:29 -0300

    test(Purchase): :test_tube: added test to ensure that purchase items exist

--- tests/application/purchase/edit-purchase-use-case.spec.ts
+++ tests/application/purchase/edit-purchase-use-case.spec.ts
@@ -1,20 +1,25 @@
+import { ApplicationError } from "@application/common";
 import { EditPurchaseUseCase } from "@application/use-cases/purchase";
 import { PaymentType } from "@domain/payment-type";
-import { MockPurchaseRepository } from "@test-application/mocks/repositories";
+import {
+  MockPurchaseItemRepository,
+  MockPurchaseRepository,
+} from "@test-application/mocks/repositories";
 import { mockCompletePurchase, mockPurchaseItem } from "@test-domain/mocks";
 
-const repository = new MockPurchaseRepository();
-const sut = new EditPurchaseUseCase(repository);
+const purchaseRepository = new MockPurchaseRepository();
+const purchaseItemRepository = new MockPurchaseItemRepository();
+const sut = new EditPurchaseUseCase(purchaseRepository, purchaseItemRepository);
 
 describe("Edit Purchase Use Case", () => {
   beforeAll(async () => {
     for (let index = 0; index < 5; index++) {
-      await repository.create(mockCompletePurchase({}));
+      await purchaseRepository.create(mockCompletePurchase({}));
     }
 
-    await repository.create(mockCompletePurchase({ id: "any_id" }));
+    await purchaseRepository.create(mockCompletePurchase({ id: "any_id" }));
 
-    await repository.create(
+    await purchaseRepository.create(
       mockCompletePurchase({
         id: "discount_id",
         items: [mockPurchaseItem({ price: 600 })],
@@ -43,7 +48,7 @@ describe("Edit Purchase Use Case", () => {
   it("should return the edited purchase on success", async () => {
     const purchase = await sut.execute({
       id: "any_id",
-      items: [mockPurchaseItem({ price: 100 })],
+      items: [mockPurchaseItem({ id: "valid_id_1", price: 100 })],
       paymentType: PaymentType.CREDIT_CARD,
     });
 
@@ -52,8 +57,8 @@ describe("Edit Purchase Use Case", () => {
   it("should return the edited purchase if only one field provided", async () => {
     const purchase = await sut.execute({
       id: "any_id",
-      items: [mockPurchaseItem({ price: 100 })],
-      paymentType: PaymentType.CASH,
+      items: [mockPurchaseItem({ id: "valid_id_1", price: 100 })],
+      paymentType: PaymentType.BOLETO,
     });
 
     expect(purchase).toBeTruthy();
@@ -61,10 +66,19 @@ describe("Edit Purchase Use Case", () => {
   it("should remove discount if total price gets below 500.00", async () => {
     const purchase = await sut.execute({
       id: "discount_id",
-      items: [mockPurchaseItem({ price: 100 })],
+      items: [mockPurchaseItem({ id: "valid_id_1", price: 100 })],
       paymentType: PaymentType.CASH,
     });
 
     expect(purchase.total).toBe(100);
+  });
+  it("should throw if invalid purchase item ids provided", async () => {
+    expect(async () => {
+      await sut.execute({
+        id: "any_id",
+        items: [mockPurchaseItem({})],
+        paymentType: PaymentType.CASH,
+      });
+    }).rejects.toThrowError(ApplicationError);
   });
 });
