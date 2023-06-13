263a990da4039fce51c01fefd25ba8eef6b949b8
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-06-13 19:28:30 -0300

    fix(Tests): :clown_face: fixed user mock params usage

--- tests/domain/mocks/mock-user.ts
+++ tests/domain/mocks/mock-user.ts
@@ -6,10 +6,12 @@ export const mockUser = (mock: Partial<UserProps>): User => {
     id: undefined,
     cpf: mock.cpf ? mock.cpf : faker.phone.number("###########"),
     email: mock.email ? mock.email : faker.internet.email(),
-    birthdate: mock.birthdate ? mock.birthdate : faker.date.past({ years: 18 }),
+    birthdate: mock.birthdate
+      ? mock.birthdate
+      : faker.date.between({ from: "2000-01-01", to: "2000-01-01" }),
     name: faker.person.fullName(),
     password: faker.internet.password(),
     phone: faker.phone.number("319########"),
-    rg: faker.phone.number("##.###.###"),
+    rg: mock.rg ? mock.rg : faker.phone.number("##.###.###"),
   });
 };
