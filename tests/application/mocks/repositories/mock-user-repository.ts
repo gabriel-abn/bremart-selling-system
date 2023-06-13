cf3a366e4b0404bdbdf555f6096499f739a8be1e
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-06-13 02:46:56 -0300

    test(User): :clown_face: implemented mock methods to repository

--- tests/application/mocks/repositories/mock-user-repository.ts
+++ tests/application/mocks/repositories/mock-user-repository.ts
@@ -1,13 +1,31 @@
+import { ApplicationError } from "@application/common";
 import { IUserRepository } from "@application/repositories";
-import { UserProps } from "@domain/user";
+import { User, UserProps } from "@domain/user";
 
 export class MockUserRepository implements IUserRepository {
+  public items: User[] = [];
+
+  async register(user: User): Promise<{ id: string }> {
+    if (this.items.find((u) => u.props.cpf == user.props.cpf)) {
+      throw new ApplicationError(
+        "CPF already exists in repository.",
+        "User Repository: "
+      );
+    }
+
+    this.items.push(user);
+    return { id: user.id };
+  }
+
   async getById(id: string): Promise<UserProps> {
-    return {
-      userId: id ? id : "1",
-      name: "John Doe",
-      email: "test@email.com",
-      cpf: "00000000000",
-    };
+    return this.items.find((u) => {
+      return u.id === id;
+    }).props;
+  }
+
+  async getByCPF(cpf: string): Promise<UserProps> {
+    return this.items.find((u) => {
+      return u.props.cpf === cpf;
+    }).props;
   }
 }
