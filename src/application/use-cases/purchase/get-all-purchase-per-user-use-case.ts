c83a181be69d7ea5d8592db1f706e4a174ccd8c2
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-06-19 22:36:29 -0300

    feat(Purchase): :sparkles: implemented 'getAllPurchasePerUser' use case

---
+++ src/application/use-cases/purchase/get-all-purchase-per-user-use-case.ts
@@ -0,0 +1,35 @@
+import { ApplicationError } from "@application/common";
+import {
+  IPurchaseRepository,
+  IUserRepository,
+} from "@application/repositories";
+import { PurchaseProps } from "@domain/purchase";
+
+export namespace GetAllPurchasePerUser {
+  export type Params = {
+    userId: string;
+  };
+
+  export type Result = PurchaseProps[];
+}
+
+export class GetAllPurchasePerUserUseCase {
+  constructor(
+    private readonly purchaseRepository: IPurchaseRepository,
+    private readonly userRepository: IUserRepository
+  ) {}
+
+  async execute({
+    userId,
+  }: GetAllPurchasePerUser.Params): Promise<GetAllPurchasePerUser.Result> {
+    const user = await this.userRepository.getById(userId);
+
+    if (!user) {
+      throw new ApplicationError("User not found", this.constructor.name);
+    }
+
+    const purchases = await this.purchaseRepository.getAllByUserId(userId);
+
+    return purchases;
+  }
+}
--- src/application/use-cases/purchase/index.ts
+++ src/application/use-cases/purchase/index.ts
@@ -1,4 +1,5 @@
 export * from "./create-purchase-use-case";
 export * from "./delete-purchase-use-case";
 export * from "./edit-purchase-use-case";
+export * from "./get-all-purchase-per-user-use-case";
 export * from "./get-purchase-use-case";
