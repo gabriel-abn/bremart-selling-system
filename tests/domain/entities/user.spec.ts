544908de3cf1680ab100c7bddb5d182750e38259
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-08-21 10:13:43 -0300

    test(User): :test_tube: added business rules tests descriptions

---
+++ tests/domain/entities/user.spec.ts
@@ -0,0 +1,9 @@
+describe("User business rules", () => {
+  it("should be able to create an user just with email and password fields", () => {});
+  it("should throw if user has less than 18 years old", () => {});
+  it("should be able to update user name", () => {});
+  it("should be able to update user password", () => {});
+  it("should set default address if just one given", () => {});
+  it("should be able to add addresses", () => {});
+  it("should be able to update user's default address if more than one given", () => {});
+});
