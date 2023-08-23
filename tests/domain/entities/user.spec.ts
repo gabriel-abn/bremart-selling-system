f453fcb54684d5ccc2e62942663904b81ef4b511
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-08-23 15:13:52 -0300

    test(User): :fire: removed fields validation test

--- tests/domain/entities/user.spec.ts
+++ tests/domain/entities/user.spec.ts
@@ -1,5 +1,4 @@
 describe("User business rules", () => {
-  it("should be able to create an user just with email and password fields", () => {});
   it("should throw if user has less than 18 years old", () => {});
   it("should be able to update user name", () => {});
   it("should be able to update user password", () => {});
