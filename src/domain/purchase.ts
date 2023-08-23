5aa427a161ab35603f98d257bc8b0dc90c499fe0
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-08-23 15:39:25 -0300

    feat(Purchase): :sparkles: implemented entity business rules

--- src/domain/purchase.ts
+++ src/domain/purchase.ts
@@ -7,7 +7,8 @@ export type PurchaseProps = {
   userId?: string;
   items: PurchaseItemProps[];
   paymentType: PaymentType;
-  voucher?: string;
+  discountPercentage: number;
+  discountValue: number;
   total?: number;
 };
 
@@ -16,18 +17,50 @@ export class Purchase extends Entity<PurchaseProps> {
     super(props, props.id);
   }
 
+  public getShoppingCart(): PurchaseItemProps[] {
+    return this.props.items;
+  }
+
+  public getTotal(): number {
+    return this.props.total;
+  }
+
   public static create(props: PurchaseProps): Purchase {
-    return new Purchase({
-      ...props,
-      total: props.items
-        .map((item) => item.price)
-        .reduce((a, b) => {
-          return (
-            a +
-            b -
-            b * (props.paymentType === PaymentType.CASH && b > 500 ? 0.05 : 0)
-          );
-        }, 0),
-    });
+    var errors: string[] = [];
+
+    if (!props.userId) {
+      errors.push("User id must be provided");
+    }
+
+    if (props.items.length <= 0) {
+      errors.push("Purchase must have at least one item");
+    }
+
+    props.total = props.items
+      .map((item) => item.price)
+      .reduce((a, b) => {
+        return a + b;
+      }, 0);
+
+    if (props.total <= 0) {
+      errors.push("Total value must be greater than 0");
+    }
+
+    if (props.paymentType === PaymentType.CASH && props.total > 500) {
+      props.total += props.total * 0.05;
+    }
+
+    if (props.discountPercentage > 1 || props.discountPercentage < 0) {
+      errors.push("Discount percentage must be between 0 and 1");
+    }
+
+    if (props.discountValue > props.total) {
+      errors.push("Discount value must be less than total value");
+    }
+
+    props.total =
+      props.total * (1 - props.discountPercentage) - props.discountValue;
+
+    return new Purchase(props);
   }
 }
