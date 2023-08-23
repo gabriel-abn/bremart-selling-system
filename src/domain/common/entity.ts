3ef750534d76af3ded542b56d2aa1d06cf885dec
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-08-23 15:16:11 -0300

    feat(Domain): :sparkles: implemented 'equals' and 'getProps' methods

--- src/domain/common/entity.ts
+++ src/domain/common/entity.ts
@@ -18,4 +18,24 @@ export abstract class Entity<T> {
       updatedAt: this.updatedAt,
     };
   };
+
+  public equals = (object?: Entity<T>): boolean => {
+    if (object == null || object == undefined) {
+      return false;
+    }
+
+    if (this === object) {
+      return true;
+    }
+
+    if (!(object instanceof Entity)) {
+      return false;
+    }
+
+    return this.id === object.id;
+  };
+
+  public getProps = (): T => {
+    return this.props;
+  };
 }
