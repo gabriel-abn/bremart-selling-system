270913485ac7f54bbbf6b2cdc8d2ff70849e6820
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-08-23 21:21:41 -0300

    test(User): :clown_face: changed mock creation

--- tests/domain/mocks/mock-user.ts
+++ tests/domain/mocks/mock-user.ts
@@ -3,15 +3,14 @@ import { faker } from "@faker-js/faker";
 
 export const mockUser = (mock: Partial<UserProps>): User => {
   return User.create({
-    id: mock.id ? mock.id : undefined,
-    cpf: mock.cpf ? mock.cpf : faker.phone.number("###.###.###-##"),
-    email: mock.email ? mock.email : faker.internet.email(),
-    birthdate: mock.birthdate
-      ? mock.birthdate
-      : faker.date.between({ from: "2000-01-01", to: "2000-01-01" }),
+    id: "ID" + Math.floor(Math.random() * 100).toString(),
+    cpf: faker.phone.number("###.###.###-##"),
+    email: faker.internet.email(),
+    birthdate: faker.date.between({ from: "2000-01-01", to: "2000-01-01" }),
     name: faker.person.fullName(),
     password: faker.internet.password(),
     phone: faker.phone.number("##9########"),
-    rg: mock.rg ? mock.rg : faker.phone.number("##.###.###"),
+    rg: faker.phone.number("##.###.###"),
+    ...mock,
   });
 };
