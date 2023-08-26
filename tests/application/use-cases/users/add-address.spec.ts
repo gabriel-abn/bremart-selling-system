b9bc5b6baaac014117ccca3a04d248229b69ad3b
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-08-26 01:26:10 -0300

    test(User): :white_check_mark: updated 'add address' tests
    Removed user's account status tests. Disabled or not verified accounts won't be able to manage accounts services.

--- tests/application/use-cases/users/add-address.spec.ts
+++ tests/application/use-cases/users/add-address.spec.ts
@@ -1,7 +1,5 @@
 describe("Add address use case", () => {
   it("should throw if user does not exist");
-  it("should throw if user is not verified");
-  it("should throw user is disabled");
   it("should throw if address already exists");
   it("should throw if address is invalid");
   it("should add address to user");
