798cd14714b8ca02b6dc76987acd059017f9e657
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2023-09-08 17:44:42 -0300

    revert(User): :rewind: removed user auth use cases

--- tests/application/use-cases/users/login.spec.ts
+++
@@ -1,7 +0,0 @@
-describe("Login use case", () => {
-  it("should throw if user email does not exist", () => {});
-  it("should throw if user email is not verified", () => {});
-  it("should throw if user password does not match", () => {});
-  it("should throw if user is not active", () => {});
-  it("should return an access token", () => {});
-});
